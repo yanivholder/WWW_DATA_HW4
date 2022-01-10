51ebc7cc8069a85202f82aaa3f80ed90835f2b53
eilon299 <eilonkornboim@gmail.com>
2022-01-10 18:39:58 +0200

    added register funcs

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask
+from flask import Flask, request, Response
 from flask_sqlalchemy import SQLAlchemy
+from models import db, User
+import crud
 
 from config import POSTGRES_CONFIG
 
@@ -8,8 +10,8 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = f'''
     postgresql://{POSTGRES_CONFIG["username"]}:{POSTGRES_CONFIG["password"]}@localhost:{POSTGRES_CONFIG["port"]}/{POSTGRES_CONFIG["name"]}
 '''.strip()
-db = SQLAlchemy(app)
 
+db.init_app(app)
 
 @app.route('/')
 def index():
@@ -18,6 +20,23 @@ def index():
 
 @app.route('/register')
 def register_request():
+    # TODO - what if request.method is not GET? should we return error? or not deal with this case
+    new_user_name = request.args['user_name']
+    chat_id = request.args['ChatId']
+
+    old_user_name = User.is_registered(chat_id)
+
+    if old_user_name is not None:
+        if old_user_name == new_user_name:
+            return Response("You've already registered under this username.", status=403)
+        else:
+            User.register_update(chat_id, new_user_name)
+            return Response(f"Successfully changed your username from {old_user_name} to {new_user_name}.", status=400)
+
+    else: # old_user_name is None
+        User.register_new(chat_id, new_user_name)
+        return Response(f"Welcome aboard, {new_user_name}.", status=400)
+
     pass
 
 
--- models.py
+++ models.py
@@ -1,6 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 
-from app import db
+db = SQLAlchemy()
 
 
 class User(db.Model):
@@ -10,4 +10,22 @@ class User(db.Model):
 
     def __repr__(self):
         return f'<User {self.username}'
+
+    def is_registered(chat_id):
+        """ checks whether chat_id is in the table Users, if so, return the current name under it, if not - none """
+        user = db.session.query(User).filter(User.id == chat_id)
+        if user is None:
+            return None
+        else:
+            return user.username
+
+    def register_new(chat_id, user_name):
+        """ does the registration in to the DB for first time registration """
+        db.session.add(User(id=chat_id, username=user_name))
+        db.session.commit()
+
+    def register_update(chat_id, user_name):
+        """ does the registration in to the DB for non-first time registration """
+        db.session.query(User).filter(User.id == chat_id).update({'username': user_name})
+        db.session.commit()
 
